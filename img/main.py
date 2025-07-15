02686715f81768ab70f541d1c8f5a701428308b8
yousefyasser <yousefyasser747@gmail.com>
2025-07-15 05:27:54 +0300

    Add files via upload

---
+++ img/main.py
@@ -0,0 +1,16 @@
+def Full_Name(first , last):
+    print(f"hello {first} {last}")
+
+
+
+name = input()
+
+Full_Name(name , 'yousef') 
+
+
+def add(num1,num2):
+    print(num1+num2)
+
+
+add(29,33)    
+    
